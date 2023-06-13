e35795dab11670db85698fcddd488c8180c8bd97
hassanulinam <hassanulinam143@gmail.com>
2023-06-13 22:56:43 +0530

    optimize `peak index of the array program`

--- 8.peakOfTheArray.ts
+++ 8.peakOfTheArray.ts
@@ -1,28 +1,31 @@
+const { question } = require("readline-sync");
+
 // Find the peak (maximum) in the Bitonic array.
 // Example bitonic array: [1, 2, 3, 4, 3, 2] (assume no duplicates will occur)
 function getPeakIndex(arr: number[]): number {
   let start = 0,
     end = arr.length - 1,
-    mid: number,
-    peak = 0;
+    mid: number;
 
-  while (start <= end) {
+  while (start < end) {
+    // start, end will eventually overlap at peak index. Hence using `<` instead of `<=`
     mid = Math.floor((start + end) / 2);
 
     if (arr[mid] > arr[mid + 1]) {
-      // we are in decreasing part
-      peak = mid;
-      end = mid - 1;
+      // this may be possible answer, but go look left
+      end = mid; // we should consider mid index as it is greater one. (rather than mid-1)
     } else {
-      // we are in increasing part
-      peak = mid + 1;
+      // here we need not consider mid index as it is smaller one.
       start = mid + 1;
     }
   }
-
-  return peak;
+  // after the loop, start and end pointers will point to a same index which is the peak one.
+  console.log({ start, end });
+  return start;
 }
 
-const givenBitonicArray = [-1, 0, 1, 2, 3, 4, 5, 2, 1];
+const givenBitonicArray = question("Enter mountain array:\n")
+  .split(" ")
+  .map((s: string) => parseInt(s));
 const peakIndex = getPeakIndex(givenBitonicArray);
 console.log({ peakIndex });
