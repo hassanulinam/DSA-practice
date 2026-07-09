52cf6f62755594c442ab5823f61c7eb9d6e54cdc
Hassan <hassanulinam143@gmail.com>
2026-07-10 00:05:07 +0530

    Longest increasing path in matrix

--- 11.DP/NewGridPattern/1.longest_increasing_path_in_matrix.py
+++ 11.DP/NewGridPattern/1.longest_increasing_path_in_matrix.py
@@ -15,58 +15,34 @@ Explanation: The longest increasing path is [1, 2, 6, 9].
 
 
 class Solution:
-    def is_in_bound(self, nr: int, nc: int, rows: int, cols: int) -> bool:
-        return nr >= 0 and nc >= 0 and nr < rows and nc < cols
-
-    def get_adjancency_matrix(self, mx: list[list[int]]) -> dict[int, list[int]]:
-        graph: dict[int, set[int]] = {}
-        m, n = len(mx), len(mx[0])
-
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-        for i in range(m):
-            for j in range(n):
-                if mx[i][j] not in graph.keys():
-                    graph[mx[i][j]] = set()
-                for dr, dc in directions:
-                    nr, nc = i + dr, j + dc
-                    if self.is_in_bound(nr, nc, m, n):
-                        graph[mx[i][j]].add(mx[nr][nc])
-
-        update_graph: dict[int, list[int]] = {}
-        for v in graph.keys():
-            update_graph[v] = list(graph[v])
-        return update_graph
+    def is_in_bound(self, nr: int, nc: int, m: int, n: int) -> bool:
+        return nr >= 0 and nc >= 0 and nr < m and nc < n
 
     def longestIncreasingPath(self, matrix: list[list[int]]) -> int:
-        graph = self.get_adjancency_matrix(matrix)
-        # maxKey = max(graph.keys())
-        memo: dict[int, int] = {}
+        if not matrix or not matrix[0]:
+            return 0
 
-        def dfs(start: int) -> int:
-            if start in memo:
-                return memo[start]
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * n for _ in range(m)]
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-            distances = [0]
-            for node in graph[start]:
-                if node < start:
-                    if node in memo:
-                        distances.append(memo[node])
-                    else:
-                        memo[node] = dfs(node)
-                        distances.append(memo[node])
-            return max(distances) + 1
+        def dfs(i: int, j: int) -> int:
+            if not dp[i][j]:
+                vals = [0]
+                for dr, dc in directions:
+                    nr, nc = dr + i, dc + j
+                    if self.is_in_bound(nr, nc, m, n) and matrix[i][j] > matrix[nr][nc]:
+                        vals.append(dfs(nr, nc))
+                dp[i][j] = 1 + max(vals)
+            return dp[i][j]
 
-        ans = 0
-        for each_key in graph.keys():
-            ans = max(ans, dfs(each_key))
-
-        return ans
+        return max(dfs(x, y) for x in range(m) for y in range(n))
 
 
 matrix = [
-    [3, 4, 5],
-    [3, 2, 6],
-    [2, 2, 1],
+    [9, 9, 4],
+    [6, 6, 8],
+    [2, 1, 1],
 ]
 sol = Solution()
 print(sol.longestIncreasingPath(matrix))
